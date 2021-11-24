d786e4fc6c9563587d3033c475978c906d452ff5
kimsunhak <kkyj3954@naver.com>
2021-11-24 17:23:55 +0900

    ** ThreadLocal -> 동시성 문제

---
+++ src/test/java/hello/advanced/common/SleepUtil.java
@@ -0,0 +1,12 @@
+package hello.advanced.common;
+
+public class SleepUtil {
+
+    public void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/hello/advanced/trace/logtrace/FiledLogTraceTest.java
@@ -0,0 +1,25 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+public class FiledLogTraceTest {
+
+    FiledLogTrace trace = new FiledLogTrace();
+
+    @Test
+    void begin_end_level2() {
+        TraceStatus status = trace.begin("hello");
+        TraceStatus statusTwo = trace.begin("helloTwo");
+        trace.end(statusTwo);
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception_level2() {
+        TraceStatus status = trace.begin("hello");
+        TraceStatus statusTwo = trace.begin("helloTwo");
+        trace.exception(statusTwo, new IllegalStateException());
+        trace.exception(status, new IllegalStateException());
+    }
+}
