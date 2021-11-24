dace0855c8f6729d875ff4e90006457ee3cc2603
kimsunhak <kkyj3954@naver.com>
2021-11-24 14:35:00 +0900

    ** 로그 추적기 V2

---
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV2Test.java
@@ -0,0 +1,26 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+public class HelloTraceV2Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status = trace.begin("hello");
+        TraceStatus status2 = trace.beginSync(status.getTraceId(), "helloTwo");
+        trace.beginSync(status.getTraceId(), "helloTwo");
+        trace.end(status);
+        trace.end(status2);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status = trace.begin("hello");
+        TraceStatus status2 = trace.beginSync(status.getTraceId(), "helloTwo");
+        trace.exception(status, new IllegalStateException());
+        trace.exception(status2, new IllegalStateException());
+    }
+}
